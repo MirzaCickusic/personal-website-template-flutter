6f384238f68070b0867d692a380067fd2a5a5012
MirzaC <cicak.mc@gmail.com>
2020-07-20 14:01:27 +0200

    First commit

--- lib/constanst/images.dart
+++ lib/constanst/images.dart
@@ -1,5 +1,6 @@
 const String logoImage = "add your logo to assets and pubspec.yaml and copy the path here";
 
+//ADD YOUR OWN ICON OR USE ICON I PROVIDED IN ASSETS
 const String linkedInImage = 'add your image to assets and pubspec.yaml and copy the path here';
 const String gitHubImage = 'add your image to assets and pubspec.yaml and copy the path here';
 const String instagramImage = 'add your image to assets and pubspec.yaml and copy the path here';
